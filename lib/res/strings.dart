48b698bd1dce46bba088e840f5f61d7bf7c45d7b
M. Waisberg <pnemonic@gmail.com>
2020-03-12 14:57:53 +0200

    strings

--- lib/res/strings.dart
+++ lib/res/strings.dart
@@ -1,9 +1,9 @@
-class _string {
+class _String {
   final app_title = 'The Movie Database Demo';
   final movie_details = 'Movie Details';
   final now_playing = 'Now Playing';
 }
 
 class R {
-  static final string = _string();
+  static final string = _String();
 }
