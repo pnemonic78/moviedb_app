315a436365d0daa0a89a23ea5f8f6553402614cc
M. Waisberg <pnemonic@gmail.com>
2020-09-22 10:55:15 +0300

    macos

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -2,6 +2,7 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/moshe/Library/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/moshe/GitHub/pnemonic78/moviedb_app"
+export "FLUTTER_TARGET=/Users/moshe/GitHub/pnemonic78/moviedb_app/lib/main/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_FRAMEWORK_DIR=/Users/moshe/Library/flutter/bin/cache/artifacts/engine/darwin-x64"
 export "FLUTTER_BUILD_NAME=1.0.0"
