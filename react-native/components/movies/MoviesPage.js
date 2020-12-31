ccb8e86bb85e7b40cb34c78c1345c08f22270e6d
M. Waisberg <pnemonic@gmail.com>
2020-12-31 09:20:55 +0200

    movies pages - fake list

--- react-native/components/movies/MoviesPage.js
+++ react-native/components/movies/MoviesPage.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Text, View, FlatList, Image, Pressable } from 'react-native';
+import axios from 'axios';
 
 const MoviesPage = ({ navigation }) => {
-    const now = new Date();
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        setMovies(["A", "B", "C"]);
+    }, []);
+
+    const renderItem = ({ item }) => {
+        return (
+            <Text>{item.toString()}</Text>
+        );
+    }
 
     return (
         <View>
-            <Text>Now: {now.toLocaleString()}</Text>
+            <FlatList
+                data={ movies }
+                renderItem={ renderItem }
+                keyExtractor={ (item, i) => i.toString() }
+                />
         </View>
     );
 };
