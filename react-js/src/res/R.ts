85ee77a896c9c298d90ee872123d3ab08f9a4c2f
M. Waisberg <pnemonic@gmail.com>
2021-06-03 10:12:05 +0300

    copy resources from react native

---
+++ react-js/src/res/R.ts
@@ -0,0 +1,14 @@
+import colors from './colors'
+import dimens from './dimens'
+import icons from './icons'
+import images from './images'
+import strings from './strings'
+
+const R = {
+  color: colors,
+  dimen: dimens,
+  drawable: images,
+  icon: icons,
+  string: strings,
+}
+export default R
---
+++ react-js/src/res/dimens.ts
@@ -0,0 +1,64 @@
+const cardRadius = 16
+
+const posterListHeight = 200.0
+const posterListWidth = posterListHeight / 1.5
+
+const posterGridHeight = 200.0
+const posterGridRatio = 1.5
+const posterGridWidth = posterGridHeight / posterGridRatio
+
+const posterDetailsHeight = 300.0
+const posterDetailsWidth = posterDetailsHeight / 1.5
+
+const thumbnailHeight = 150.0
+const thumbnailWidth = thumbnailHeight * 16 / 9
+
+const backdropHeight = 300.0
+const backdropWidth = backdropHeight * 16 / 9
+
+const castTileWidth = 150
+const castTileHeight = castTileWidth * 1.5
+
+const personDetailsHeight = 300.0
+const personDetailsWidth = personDetailsHeight / 1.5
+
+const personIconSize = personDetailsWidth / 6
+
+const errorIconSize = 100.0
+
+const actionBarButtonMargin = 8
+const actionBarButtonSize = 34
+
+const dimens = {
+    cardRadius: cardRadius,
+
+    posterListHeight: posterListHeight,
+    posterListWidth: posterListWidth,
+
+    posterGridWidth: posterGridWidth,
+    posterGridRatio: posterGridRatio,
+    posterGridHeight: posterGridHeight,
+
+    posterDetailsHeight: posterDetailsHeight,
+    posterDetailsWidth: posterDetailsWidth,
+
+    thumbnailHeight: thumbnailHeight,
+    thumbnailWidth: thumbnailWidth,
+
+    backdropHeight: backdropHeight,
+    backdropWidth: backdropWidth,
+
+    castTileHeight: castTileHeight,
+    castTileWidth: castTileWidth,
+
+    personDetailsHeight: personDetailsHeight,
+    personDetailsWidth: personDetailsWidth,
+
+    personIconSize: personIconSize,
+
+    errorIconSize: errorIconSize,
+
+    actionBarButtonMargin: actionBarButtonMargin,
+    actionBarButtonSize: actionBarButtonSize,
+}
+export default dimens
