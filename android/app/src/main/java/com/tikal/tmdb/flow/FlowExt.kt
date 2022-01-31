29d8543f0e6ffc5c34289e920e3ace06c4b97551
Moshe Waisberg <Moshe.Waisberg@nike.com>
2022-01-31 12:09:23 +0200

    migrate RxJava to Kotlin Flow

--- android/app/src/main/java/com/tikal/tmdb/flow/FlowExt.kt
+++ android/app/src/main/java/com/tikal/tmdb/flow/FlowExt.kt
@@ -1,7 +1,11 @@
 package com.tikal.tmdb.flow
 
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flattenConcat
-import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.emitAll
+import kotlinx.coroutines.flow.flow
 
-fun <T> concat(vararg elements: Flow<T>): Flow<T> = flowOf(*elements).flattenConcat()
+fun <T> concat(vararg elements: Flow<T>): Flow<T> = flow {
+    for (element in elements) {
+        emitAll(element)
+    }
+}
