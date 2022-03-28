4c2f197a5d591a84bdf9ff5ee8f6cdbc9c934f38
Moshe Waisberg <Moshe.Waisberg@nike.com>
2022-03-28 17:04:33 +0300

    latest dependencies

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -60,24 +60,24 @@ dependencies {
 
     // Jetpack
     implementation("androidx.appcompat:appcompat:1.4.1")
-    implementation("androidx.compose.material:material:1.2.0-alpha02")
-    implementation("androidx.compose.runtime:runtime-livedata:1.0.5")
-    implementation("androidx.compose.ui:ui-tooling:1.2.0-alpha02")
-    implementation("androidx.compose.ui:ui:1.2.0-alpha02")
+    implementation("androidx.compose.material:material:1.2.0-alpha06")
+    implementation("androidx.compose.runtime:runtime-livedata:1.2.0-alpha06")
+    implementation("androidx.compose.ui:ui-tooling:1.2.0-alpha06")
+    implementation("androidx.compose.ui:ui:1.2.0-alpha06")
     implementation("androidx.constraintlayout:constraintlayout:2.1.3")
     implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.4.0")
-    implementation("androidx.navigation:navigation-ui-ktx:2.4.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.4.1")
+    implementation("androidx.navigation:navigation-ui-ktx:2.4.1")
 
     // JSON
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.0")
     implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:0.8.0")
 
     // Logging
-    implementation("com.jakewharton.timber:timber:4.7.1")
+    implementation("com.jakewharton.timber:timber:5.0.1")
 
     // Network
-    implementation("com.squareup.okhttp3:logging-interceptor:4.9.1")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.9.3")
     implementation("com.squareup.okhttp3:okhttp:4.9.3")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
 
