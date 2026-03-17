aa0a78b3d9762e81617163bfd7c8c26eb56ed36d
Moshe W <pnemonic@gmail.com>
2026-03-17 10:10:46 +0200

    rn - not web

--- react-native/src/app/index.tsx
+++ react-native/src/app/index.tsx
@@ -6,13 +6,9 @@ import { AnimatedIcon } from '@/components/animated-icon';
 import { HintRow } from '@/components/hint-row';
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
-import { WebBadge } from '@/components/web-badge';
 import { BottomTabInset, MaxContentWidth, Spacing } from '@/constants/theme';
 
 function getDevMenuHint() {
-  if (Platform.OS === 'web') {
-    return <ThemedText type="small">use browser devtools</ThemedText>;
-  }
   if (Device.isDevice) {
     return (
       <ThemedText type="small">
@@ -54,8 +50,6 @@ export default function HomeScreen() {
             hint={<ThemedText type="code">npm run reset-project</ThemedText>}
           />
         </ThemedView>
-
-        {Platform.OS === 'web' && <WebBadge />}
       </SafeAreaView>
     </ThemedView>
   );
